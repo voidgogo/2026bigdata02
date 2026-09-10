c499a6b1e0bfbe62b4d5f57f56793bc32663d548
voidgogo <2021038@daelim.ac.kr>
2026-09-10 11:32:10 +0900

    week02 v0.1

--- week02/week02.py
+++ week02/week02.py
@@ -1,3 +1,10 @@
 import numpy as np
 import pandas as pd
 
+df1 = pd.read_csv("./bike.csv")
+#print(df1.head())
+#print(df1.info())
+#print(df1['temp'])
+print(df1.loc[df1['season'] != 1])
+#print(df1.loc[3:7,'workingday':'atemp'])
+#print(df1.iloc[3:8,3:7])
