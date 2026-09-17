7f6ad4005b8ee94b6cf3337d71a3f1b697eda8c5
voidgogo <2021038@daelim.ac.kr>
2026-09-17 11:45:32 +0900

    week03 v0.2

--- week03/week03.py
+++ week03/week03.py
@@ -1,12 +1,24 @@
 import numpy as np
 import pandas as pd
 
-df1 = pd.read_csv("./bike.csv")
+df2 = pd.read_csv("./bookings.csv")
+# print(df2.info())
+# print(df2['Review'])
+print(df2['Review'].value_counts())
+df2.loc[df2['Review'] == "Superb 9.0", "Review"] = "Superb"
+df2.loc[df2['Review'] == "Exceptional 10", "Review"] = "Exceptional"
+df2.loc[df2['Review'] == "Superb ", "Review"] = "Superb"  # 띄어쓰기 제거
+df2.loc[df2['Review'] == "Exceptional ", "Review"] = "Exceptional"  # 띄어쓰기 제거
+df2.loc[df2['Review'] == "Good ", "Review"] = "Good"  # 띄어쓰기 제거
+df2.loc[df2['Review'] == "Very good ", "Review"] = "Very good"  # 띄어쓰기 제거
+df2.loc[df2['Review'] == "Fabulous ", "Review"] = "Fabulous"  # 띄어쓰기 제거
+df2.loc[df2['Review'] == "Review score ", "Review"] = "Review score"  # 띄어쓰기 제거
 
-# df1 = df1.rename({'registered':'registered_user','casual':'casual_user'}, axis=1)
-df1.rename({'registered':'registered_user','casual':'casual_user'}, axis=1, inplace=True)
-# print(df1.head())
-# print(df1.info())
-# print(df1.describe(include='str'))
-# print(df1.describe(include='float'))
-print(df1.describe(exclude='int'))
+print(df2['Review'].value_counts())
+
+# print(df2[df2['Review'] == 'Review score'])  # 띄어쓰기 주의
+print(df2.loc[df2['Review'] == 'Review score', ['Review','Rating']])
+# print(df2.loc[df2['Review'] == 'Very good', ['Review','Rating']])
+# print(df2.loc[df2['Review'] == 'Good', ['Review','Rating']])
+df2.loc[df2['Review'] == 'Review score', 'Review'] = "Good"
+print(df2['Review'].value_counts())
